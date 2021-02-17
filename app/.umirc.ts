3e806ccdefaf428389d6fcdd4ed881b9c6c16d0e
lvnuoyan <57041896+evalny@users.noreply.github.com>
2021-02-17 21:28:27 +0800

    update for test

---
+++ app/.umirc.ts
@@ -0,0 +1,47 @@
+import { IConfig } from 'umi-types';
+
+// ref: https://umijs.org/config/
+const config: IConfig =  {
+  proxy: {
+    '/jsonrpc': {
+      'target': 'http://127.0.0.1:8001/jsonrpc/',
+      'changeOrigin': true,
+    },
+  },
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index' },
+        { path: '/a', component: '../pages/a' }
+      ]
+    }
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: { webpackChunkName: true },
+      title: 'learning01',
+      dll: true,
+      locale: {
+        enable: true,
+        default: 'en-US',
+      },
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
+
+export default config;
---
+++ app/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './index.css';
+
+import { Link } from 'umi';
+export default () => (
+  <div>
+    <Link to="/a">Users Page</Link>
+  </div>
+);
